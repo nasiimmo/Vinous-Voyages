a7c557071c8edfc1238b092955a158701ea6a05e
Nasiim Nuur <nasiimmo.691@gmail.com>
2023-12-15 13:02:54 +0000

    deploying

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ import mongoose from 'mongoose'
 import 'dotenv/config'
 import router from './config/routes.js'
 
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// The below line is the router, this should already be present and doesn't need to be added. Add everything below it.
+app.use('/api', router) 
+
+// ** New lines **
+app.use(express.static(path.join(__dirname, 'client', 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+})
+
 const app = express()
 
 app.use(express.json())
@@ -12,7 +28,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use('/api', router)
 
 async function startServer() {
   try {
